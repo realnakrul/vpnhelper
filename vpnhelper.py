42b79dc496abde18c38b102bc3a7af58f0cfca30
realnakrul <na_krul@mail.ru>
2020-05-05 21:41:12 +0400

    Some progress

--- vpnhelper.py
+++ vpnhelper.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
+
+
 import platform
-import os
 from pprint import pprint
+import subprocess
 
 
 SERVERS = {'Russia': [{'Russia': 'ru.asn247.net'}],
@@ -11,7 +14,7 @@ SERVERS = {'Russia': [{'Russia': 'ru.asn247.net'}],
 def check_platform():
     current_platform = platform.system()
     result = False
-    if current_platform == 'Linux':
+    if current_platform != 'Linux':
         print(f"{current_platform} not supported yet")
     else:
         print(f"{current_platform} supported")
@@ -20,11 +23,23 @@ def check_platform():
 
 
 def main():
-    os.system('dir')
+    user, password = get_credentials()
+    print(user, password)
+    subprocess.run('dir')
+
+
+def get_credentials():
+    user = 'A'
+    password = 'B'
+    try:
+        pass  #TODO: Read credentials from file
+    except Exception as e:
+        print(type(e))
+    #TODO: if file not found request credentials
+    return user, password
 
 
 if __name__ == '__main__':
     pprint(SERVERS)
-    print(platform.system())
     if check_platform():
         main()
